061b7651e8412891d076ef6c16a6894bec81a5f4
Tann <tianen.chen@outlook.com>
2018-06-15 09:44:22 -0400

    add zihao's task

--- code/biz-user/src/main/java/com/user/controller/RoleController.java
+++ code/biz-user/src/main/java/com/user/controller/RoleController.java
@@ -6,37 +6,44 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping(value = "/r")
 public class RoleController {
 
-    //return a new list
+    //input: role, description
+    //output: list of all roles
+    //add prepersist in entity to control default value
     public Result addRole() {
         return null;
     }
 
-
+    //input: id of the role
+    //output: details includes id
     public Result getRoleDetails() {
         return null;
     }
 
-
+    //no input
+    //output: list of all roles
+    //support paging func
     public Result getRoleList(){
         return null;
     }
 
-
+    //input role, description, is_avail
+    //output : the role after update
     public Result updateRoleDetails() {
         return null;
     }
 
     //return a new list
+    //actually: we dont delete the record actually, change is_avail status
+    //output : return new list
     public Result deleteRole(){
         return null;
     }
 
+    // do it later
     public Result addPermissionsForRole() {
         return null;
     }
-
-
+    
 }
