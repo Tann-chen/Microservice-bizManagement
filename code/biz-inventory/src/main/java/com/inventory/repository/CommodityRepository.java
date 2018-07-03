f6c73812b3fc93782f10a87e9450fda51e2ceab4
Tann <tianen.chen@outlook.com>
2018-07-02 20:11:23 -0400

    merge all

--- code/biz-inventory/src/main/java/com/inventory/repository/CommodityRepository.java
+++ code/biz-inventory/src/main/java/com/inventory/repository/CommodityRepository.java
@@ -3,13 +3,9 @@ package com.inventory.repository;
 import com.inventory.domain.entity.Commodity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-import java.util.List;
+public interface CommodityRepository extends PagingAndSortingRepository<Commodity, Long> {
 
-
-public interface CommodityRepository extends CrudRepository<Commodity, Long> {
-
-    List<Commodity> findCommoditiesByIsAvailableTrue(Pageable pageable);
+    Page<Commodity> findCommoditiesByIsAvailableTrue(Pageable pageable);
 }
